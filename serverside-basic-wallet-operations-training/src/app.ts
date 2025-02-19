f616f3211c2892827a5440e7e67bd1442e077242
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-02-19 22:18:59 +0100

    start wallet address backend with initial routes structure

---
+++ serverside-basic-wallet-operations-training/src/app.ts
@@ -0,0 +1,1 @@
+console.log("venga, fumate uno!")
---
+++ wallet-address-book-backend/src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+
+import contactsRoutes from "./routes/contacts";
+import contactsBookRoutes from "./routes/contacts-book";
+import userRoutes from "./routes/user";
+
+const app = express();
+app.use(cors());
+
+app.use('/user', userRoutes);
+app.use('/contacts_book', contactsBookRoutes);
+app.use('/contacts', contactsRoutes);
+
+app.listen(process.env.PORT ?? 3000, () => {
+  console.log(`Server running at port ${process.env.PORT ?? 3000}`);
+});

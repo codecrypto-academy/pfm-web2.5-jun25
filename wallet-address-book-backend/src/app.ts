8eef89ec93bc4ab0b98fbcc94b7a7eb891979753
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-03-02 00:50:44 +0100

    clean up no needed routes

--- wallet-address-book-backend/src/app.ts
+++ wallet-address-book-backend/src/app.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 
-import contactsRoutes from "./routes/contacts";
 import contactsBookRoutes from "./routes/contacts-book";
 import userRoutes from "./routes/user";
 
@@ -10,7 +9,6 @@ app.use(cors());
 
 app.use("/user", userRoutes);
 app.use("/contacts_book", contactsBookRoutes);
-app.use("/contacts", contactsRoutes);
 
 app.listen(process.env.PORT ?? 3000, () => {
   console.log(`Server running at port ${process.env.PORT ?? 3000}`);

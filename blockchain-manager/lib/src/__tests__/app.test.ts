63617367156976232bf64d4917df70ce7d65a46c
JManuel Velasco <jmanuel.velasco@gmail.com>
2025-07-06 01:13:27 +0200

    refactor create docker network feature to be testable and implement different test cases

--- blockchain-manager/lib/src/__tests__/app.test.ts
+++ blockchain-manager/lib/src/__tests__/app.test.ts
@@ -1,5 +1,79 @@
-describe('App', () => {
-    it('should create a network', () => {
-        expect(true).toBe(true);
+import Docker from "dockerode";
+import { ensureNetworkExists, PROJECT_LABEL } from "../app";
+
+
+const NETWORK_ID = "22be93d5babb089c5aab8dbc369042fad48ff791584ca2da2100db837a1c7c30";
+
+describe('ensureNetworkExists', () => {
+
+    const networkConfigStub = { name: "besu-network", subnet: "172.25.0.0/16", gateway: "172.25.0.1" };
+
+    it('should create a network when it does not exist', async () => {
+        const docker = new Docker();
+        jest.spyOn(docker, 'listNetworks').mockResolvedValue([]);
+        jest.spyOn(docker, 'createNetwork').mockResolvedValue({
+            id: NETWORK_ID,
+        } as Partial<Docker.Network> as Docker.Network);
+
+        const networkId = await ensureNetworkExists(docker, networkConfigStub);
+
+        expect(networkId).toBe(NETWORK_ID);
+        expect(docker.listNetworks).toHaveBeenCalledWith({
+            filters: { name: [networkConfigStub.name] }
+        });
+        expect(docker.createNetwork).toHaveBeenCalledWith({
+            "Name": networkConfigStub.name,
+            "Driver": "bridge",
+            "CheckDuplicate": true,
+            "IPAM": {
+                "Driver": "default",
+                "Config": [
+                    {
+                        "Subnet": networkConfigStub.subnet,
+                        "Gateway": networkConfigStub.gateway
+                    }
+                ]
+            },
+            "Labels": {
+                "project": PROJECT_LABEL,
+            }
+        });
+    });
+
+    it('should return the network id when the network already exists', async () => {
+        const docker = new Docker();
+        jest.spyOn(docker, 'listNetworks').mockResolvedValue([
+            {
+                Name: 'besu-network',
+                Id: NETWORK_ID
+            } as Partial<Docker.NetworkInspectInfo> as Docker.NetworkInspectInfo
+        ]);
+        jest.spyOn(docker, 'createNetwork').mockResolvedValue({
+            id: NETWORK_ID,
+        } as Partial<Docker.Network> as Docker.Network);
+
+        const networkId = await ensureNetworkExists(docker, networkConfigStub);
+
+        expect(networkId).toBe(NETWORK_ID);
+        expect(docker.listNetworks).toHaveBeenCalledWith({
+            filters: { name: [networkConfigStub.name] }
+        });
+        expect(docker.createNetwork).not.toHaveBeenCalled();
+    });
+
+    it('should throw error when listNetworks fails', async () => {
+        const docker = new Docker();
+        jest.spyOn(docker, 'listNetworks').mockRejectedValue(new Error('Docker API error'));
+
+        await expect(ensureNetworkExists(docker, networkConfigStub)).rejects.toThrow('Docker API error');
+
+    });
+
+    it('should throw error when createNetwork fails', async () => {
+        const docker = new Docker();
+        jest.spyOn(docker, 'listNetworks').mockResolvedValue([]);
+        jest.spyOn(docker, 'createNetwork').mockRejectedValue(new Error('Docker API error'));
+
+        await expect(ensureNetworkExists(docker, networkConfigStub)).rejects.toThrow('Docker API error');
     });
 });
--- blockchain-manager/lib/src/app.ts
+++ blockchain-manager/lib/src/app.ts
@@ -1,48 +1,57 @@
 import Docker from "dockerode";
 
 
+const PROJECT_LABEL = 'besu-blockchain';
+
 const docker = new Docker();
 
-const createNetworkPayload = {
-    "Name": "besu-network",
-    "Driver": "bridge",
-    "CheckDuplicate": true,
-    "IPAM": {
-        "Driver": "default",
-        "Config": [
-            {
-                "Subnet": "172.25.0.0/16",
-                "Gateway": "172.25.0.1"
+
+async function ensureNetworkExists(docker: Docker, networkConfig: { name: string; subnet: string; gateway: string }) {
+    const networkPayload = {
+        "Name": networkConfig.name,
+        "Driver": "bridge",
+        "CheckDuplicate": true,
+        "IPAM": {
+            "Driver": "default",
+            "Config": [
+                {
+                    "Subnet": networkConfig.subnet,
+                    "Gateway": networkConfig.gateway
+                }
+            ]
+        },
+        "Labels": {
+            "project": PROJECT_LABEL,
+        }
+    }
+
+    try {
+        const network = await docker.listNetworks({
+            filters: {
+                name: [networkConfig.name]
             }
-        ]
-    },
-    "Labels": {
-        "project": "besu-blockchain",
-        "environment": "development"
+        });
+        if (network.length > 0) {
+            return network[0].Id;
+        }
+
+        const response = await docker.createNetwork(networkPayload);
+        return response.id;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        }
+        throw error;
     }
 }
 
-// Step 1: Create the network if it doesn't exist
-docker.listNetworks({
-    filters: {
-        name: [createNetworkPayload.Name]
-    }
-}, (err, networks) => {
-    if (err) {
-        console.error(err);
-    } else {
-        if (networks?.length) {
-            console.log('Network already exists');
-            return;
-        }
-        console.log('Creating network', createNetworkPayload.Name);
 
-        docker.createNetwork(createNetworkPayload, (err, network) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('network created', network);
-            }
-        });
-    }
-});
+(async () => {
+    await ensureNetworkExists(docker, { name: "besu-network", subnet: "172.25.0.0/16", gateway: "172.25.0.1" });
+})();
+
+
+export {
+    PROJECT_LABEL,
+    ensureNetworkExists,
+}
